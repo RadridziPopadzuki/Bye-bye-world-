474130367e9139643da7d0eeced25e42d2232bd7
RadridziPopadzuki <xyjail@gmail.com>
2023-04-02 19:59:52 +0300

    Create new folder Main, create new files Main.js and Main.css, replase code from App.js and App.css in this new files. Replace logo.svg from folder src in folder header. Change name of class in Header.js to Header,name beefore was App.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './header/Header';
+import Main from './main/Main';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />;
-        <main className='main'>
-          <p>
-            This HTML file is a template.
-            If you open it directly in the browser, you will see an empty page.
-
-            You can add webfonts, meta tags, or analytics to this file.
-            The build step will place the bundled scripts into the tag.
-
-            To begin the development, run `npm start` or `yarn start`.
-            To create a production bundle, use `npm run build` or `yarn build`.
-          </p>
-
-        </main>
+        <Main />;
         <footer className='footer'>Made by <a href='#'>Radridzi</a></footer>
       </div>
     );
--- src/header/Header.js
+++ src/header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Header.css';
 
 
-class App extends React.Component {
+class Header extends React.Component {
    render() {
       return (
          <header className="App-header">
