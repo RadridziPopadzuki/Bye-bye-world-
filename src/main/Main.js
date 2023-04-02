1bd3e7611ec1bc521e91f8f1a9972ea584094d95
RadridziPopadzuki <xyjail@gmail.com>
2023-04-02 20:55:41 +0300

    Add in Main.js className 'container', add in Main.js style properties

--- src/main/Main.js
+++ src/main/Main.js
@@ -5,18 +5,21 @@ import './Main.css';
 class Main extends React.Component {
    render() {
       return (
-         <main className='main'>
-            <p>
-               This HTML file is a template.
-               If you open it directly in the browser, you will see an empty page.
+         <div className='container' >
+            <main className='main' style={{fontSize: "1.5rem", background: "#435049",
+         borderRadius: "10px"}}>
+               <p>
+                  This HTML file is a template.
+                  If you open it directly in the browser, you will see an empty page.
 
-               You can add webfonts, meta tags, or analytics to this file.
-               The build step will place the bundled scripts into the tag.
+                  You can add webfonts, meta tags, or analytics to this file.
+                  The build step will place the bundled scripts into the tag.
 
-               To begin the development, run `npm start` or `yarn start`.
-               To create a production bundle, use `npm run build` or `yarn build`.
-            </p>
-         </main>
+                  To begin the development, run `npm start` or `yarn start`.
+                  To create a production bundle, use `npm run build` or `yarn build`.
+               </p>
+            </main>
+         </div>
       );
    }
 }
