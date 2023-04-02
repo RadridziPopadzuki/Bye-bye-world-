3aee0074a7016827664d2d533ce9187fbd392a94
RadridziPopadzuki <xyjail@gmail.com>
2023-04-02 19:41:21 +0300

    replaced the function Haeder with a class Header

--- src/Header.js
+++ src/Header.js
@@ -3,23 +3,25 @@ import React from 'react';
 import './Header.css';
 
 
-function Header() {
-   return (
-      <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <p>
-            Edit <code>src/App.js</code> and save to reload.
-         </p>
-         <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-         >
-            Learn React
-         </a>
-      </header>
-   );
+class App extends React.Component {
+   render() {
+      return (
+         <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+               Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+               className="App-link"
+               href="https://reactjs.org"
+               target="_blank"
+               rel="noopener noreferrer"
+            >
+               Learn React
+            </a>
+         </header>
+      );
+   }
 }
 
 export default Header;
