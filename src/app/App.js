1863ed948d2ffb9e0be4a9c87d84d01b797f7473
RadridziPopadzuki <xyjail@gmail.com>
2023-04-03 18:16:38 +0300

    Add CSS properties

--- src/app/App.js
+++ src/app/App.js
@@ -8,9 +8,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header />;
-        <Main />;
-        <Footer />;
+        <Header />
+        <Main />
+        <Footer />
       </div>
     );
   }
