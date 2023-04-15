0e4fdbe158296295808d94a73dfd0863a60c1dfe
RadridziPopadzuki <xyjail@gmail.com>
2023-04-15 20:34:17 +0300

    Add button in App.js with state and function 'push'

--- src/app/App.js
+++ src/app/App.js
@@ -11,16 +11,24 @@ class App extends React.Component {
     super(props);
     this.state = {
       h1: "Hello",
-      h2: "blya"
+      h2: "blya",
+      message: ""
     }
+  }
+  push = () => {
+    this.setState({ message: "you push the button" })
   }
   render() {
     return (
       <div className="App">
         <Nav let navItem={this.props.navItem} />
-        <div style={{ marginTop: "3rem", display: "flex", position: "relative", left: "45%" }}>
+        <div style={{ marginTop: "3rem", width: "40%", display: "flex", justifyContent: 'space-between', position: "relative", left: "30%" }}>
           <h1 style={{ color: "blanchedalmond" }}>{this.state.h1}</h1>
           <h2 style={{ color: "blanchedalmond" }}>{this.state.h2}</h2>
+        </div>
+        <div style={{ color: "red" }}>
+          <button type='button' onClick={this.push}>Push the button</button>
+          <p>{this.state.message}</p>
         </div>
         <Header let title={this.props.title} />
         <Main />
